f39e175401714aba5789ce462e557a9182079276
3vinicius <amorimvinicius3@gmail.com>
2023-11-23 14:23:51 -0300

    initial configuration calendar

---
+++ javascript/fetch.js
@@ -0,0 +1,16 @@
+function fatchfor(path){
+  const contentRoot = document.querySelector("#root");
+  const caminhoCompleto = path; // Certifique-se de que este caminho está correto
+fetch(caminhoCompleto)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar a página: ${response.status} ${response.statusText}`);
+        }
+        return response.text();
+    })
+    .then(html => {
+        contentRoot.innerHTML = html;
+    })
+    .catch(error => console.error(error));
+}
+
---
+++ javascript/render.js
@@ -0,0 +1,18 @@
+function MyComponent(container) {
+  this.container = container;
+  this.render();
+}
+
+MyComponent.prototype.render = function () {
+  this.container.innerHTML = fatchfor('pages/login.html');
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const appContainer = document.getElementById('root');
+  if (appContainer) {
+    const myComponent = new MyComponent(appContainer);
+  } else {
+    console.error('Elemento com ID "app" não encontrado no DOM.');
+  }
+});
